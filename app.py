097871c4d850679d671db05542043c37796cb271
Nandini42617 <nandinigoel325@gmail.com>
2026-07-24 16:16:30 +0530

    Change developer name from Chandan Saroj to Nandini Goel
    Updated developer details to reflect new author.

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ with gr.Blocks(css=custom_css, title="Property Rent Predictor") as interface:
             # Right Column: Developer Details & Tools
             with gr.Column(scale=1):
                 gr.Markdown("### 👨‍💻 About the Developer")
-                gr.Markdown("**Chandan Saroj**<br>Software Development Engineer & MERN Stack Developer")
-                gr.Markdown("🔗 [LinkedIn Profile](https://www.linkedin.com/in/chandan-saroj/)<br>📸 Instagram: `_chandan_saroj`")
+                gr.Markdown("**Nandini Goel**)
+                
                 
                 gr.Markdown("### 🛠️ Tools Used")
                 gr.Markdown("""
