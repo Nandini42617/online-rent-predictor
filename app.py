dc7770a25b3fe845c24a9132dde7e91a89b96a30
Nandini42617 <nandinigoel325@gmail.com>
2026-07-24 16:57:58 +0530

    Fix formatting of developer name in app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ with gr.Blocks(css=custom_css, title="Property Rent Predictor") as interface:
             # Right Column: Developer Details & Tools
             with gr.Column(scale=1):
                 gr.Markdown("### 👨‍💻 About the Developer")
-                gr.Markdown("**Nandini Goel**)
+                gr.Markdown("**Nandini Goel**")
                 
                 
                 gr.Markdown("### 🛠️ Tools Used")
